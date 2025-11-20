53f69a5dd84c79142db0fc3eb3f619702962954b
Marius <mnun@itu.dk>
2025-11-20 13:29:16 +0100

    input files added and imported

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
 
 import itumulator.executable.DisplayInformation;
 import itumulator.executable.Program;
@@ -8,7 +10,18 @@ import itumulator.world.World;
 public class Main {
 
     public static void main(String[] args) {
-        int size = 5;
+        int size = 1;
+
+        File input1 = new File("src/Data/week-1/t1-1a.txt");
+        System.out.println(System.getProperty("user.dir"));
+        try(Scanner sc = new Scanner(input1)){
+            String line1 = sc.nextLine();
+            System.out.println(line1);
+            size = Integer.parseInt(line1);
+
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
         Program p = new Program(size, 800, 75);
 
         World w = p.getWorld();
